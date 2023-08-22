bfb8299f66cfc09946f657917e25c6c905fb2e1c
emilioom95 <133070442+emilioom95@users.noreply.github.com>
2023-08-21 22:21:44 -0500

    Add files via upload

---
+++ ExamenFinal/ExFinal_POO.py
@@ -0,0 +1,106 @@
+import tkinter as tk
+import random
+import mysql.connector
+from tkinter import messagebox
+from InterfazPreguntas import CrearPreguntaApp
+
+root_style = {"bg": "blue"}
+label_style = {"font": ("Arial", 16), "bg": "blue", "fg": "white"}
+entry_style = {"font": ("Arial", 14)}
+button_style = {"font": ("Arial", 14), "bg": "green", "fg": "white", "width": 15}
+
+
+class PreguntasApp:
+    def __init__(self, root, nombre):
+        self.root = root
+        self.nombre = nombre
+        self.preguntas_correctas = 0
+        self.preguntas_incorrectas = 0
+        self.pregunta_label = tk.Label(root,font=("Helvetica", 14), bg="purple", fg="white", text="")
+        self.respuestas_buttons = [tk.Button(root, text="",font=("Helvetica", 14), bg="red", fg="white", command=lambda i=i: self.verificar_respuesta(i)) for i in range(4)]
+        self.resultado_label = tk.Label(root, text="")
+        self.result_button = tk.Button(root, text="Ver mis resultados",font=("Helvetica", 14), bg="blue", fg="white", command=self.mostrar_resultados)
+        self.salir_button = tk.Button(root, text="Salir",font=("Helvetica", 14), bg="pink", fg="white", command=exit)
+
+        self.pregunta_label.pack(pady=10)
+        for button in self.respuestas_buttons:
+            button.pack(pady=5)
+        self.resultado_label.pack(pady=10)
+        self.result_button.pack(pady=5)
+        self.salir_button.pack(pady=5)
+
+        self.conexion = mysql.connector.connect(
+            host="127.0.0.1",
+            user="root",
+            password="Emilio12345.",
+            database="neoguias"
+        )
+        self.cursor = self.conexion.cursor()
+
+        self.obtener_nueva_pregunta()
+
+    def obtener_nueva_pregunta(self):
+        pregunta, opciones = self.obtener_pregunta_aleatoria_db()
+        self.respuesta_correcta = opciones[0]
+        random.shuffle(opciones)  # Barajar las opciones
+        self.pregunta_actual = pregunta
+        self.pregunta_label.config(text=pregunta)
+        for i, button in enumerate(self.respuestas_buttons):
+            button.config(text=opciones[i])
+        self.resultado_label.config(text="")
+
+    def verificar_respuesta(self, opcion_seleccionada):
+        if self.respuestas_buttons[opcion_seleccionada]['text'] == self.respuesta_correcta:
+            tk.messagebox.showinfo("¡Correcto!", "Tu respuesta es correcta.")
+            self.preguntas_correctas += 1
+        else:
+            tk.messagebox.showerror("¡Incorrecto!", "Tu respuesta es incorrecta.")
+            self.preguntas_incorrectas += 1
+
+        self.obtener_nueva_pregunta()
+
+    def mostrar_resultados(self):
+        tk.messagebox.showinfo("Resultados", f"{self.nombre}, tus resultados son:\nPreguntas correctas: {self.preguntas_correctas}\nPreguntas incorrectas: {self.preguntas_incorrectas}")
+        self.conexion.close()
+        self.root.destroy()
+        
+    def obtener_pregunta_aleatoria_db(self):
+        self.cursor.execute("SELECT pregunta, respuesta_correcta, opcion2, opcion3, opcion4 FROM table_preguntas ORDER BY RAND() LIMIT 1")
+        pregunta, respuesta_correcta, opcion2, opcion3, opcion4 = self.cursor.fetchone()
+        return pregunta, [respuesta_correcta, opcion2, opcion3, opcion4]
+
+
+def abrir_interfaz_preguntas():
+    nombre = nombre_entry.get()
+    if nombre:
+        root.withdraw()  # Oculta la ventana actual
+        root_preguntas = tk.Tk()
+        root_preguntas.geometry("400x400")
+        root_preguntas.title("Juego de Preguntas")
+        app_preguntas = PreguntasApp(root_preguntas, nombre)
+        root_preguntas.mainloop()
+    else:
+        tk.messagebox.showerror("Error", "Debes ingresar un nombre.")
+
+def crear_nuevas_preguntas():
+    root = tk.Tk()
+    root.geometry("400x400")
+    app = CrearPreguntaApp(root)
+    root.mainloop()
+    
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.geometry("400x400")
+    root.title("Inicio")
+    
+    nombre_label = tk.Label(root, text="Ingresa tu nombre:")
+    nombre_entry = tk.Entry(root)
+    ingresar_button = tk.Button(root, text="Ingresar",font=("Helvetica", 14), bg="green", fg="white", command=abrir_interfaz_preguntas)
+    ingresarpreguntas_button = tk.Button(root, text="Ingresar nuevas preguntas",font=("Helvetica", 14), bg="gray", fg="white", command=crear_nuevas_preguntas)
+
+    nombre_label.pack(pady=10)
+    nombre_entry.pack(pady=5)
+    ingresar_button.pack(pady=10)
+    ingresarpreguntas_button.pack(pady=10)
+    root.configure(bg="blue")
+    root.mainloop()
---
+++ ExamenFinal/InterfazPreguntas.py
@@ -0,0 +1,78 @@
+import tkinter as tk
+import mysql.connector
+from tkinter import messagebox
+
+class CrearPreguntaApp:
+    def __init__(self, root):
+        self.root = root
+        self.pregunta_label = tk.Label(root, text="Pregunta:")
+        self.pregunta_entry = tk.Entry(root)
+        self.respuesta_correcta_label = tk.Label(root, text="Respuesta correcta:")
+        self.respuesta_correcta_entry = tk.Entry(root)
+        self.opciones_labels = [tk.Label(root, text=f"Opción {i+2}:") for i in range(3)]
+        self.opciones_entries = [tk.Entry(root) for _ in range(3)]
+        self.agregar_button = tk.Button(root, text="Agregar Pregunta", command=self.agregar_pregunta)
+        self.salir_button = tk.Button(root, text="Salir", command=self.cerrar_app)
+        
+        self.pregunta_label.pack(pady=10)
+        self.pregunta_entry.pack(pady=5)
+        self.respuesta_correcta_label.pack(pady=5)
+        self.respuesta_correcta_entry.pack(pady=5)
+
+        for i in range(3):
+            self.opciones_labels[i].pack(pady=5)
+            self.opciones_entries[i].pack(pady=5)
+
+        self.agregar_button.pack(pady=10)
+        self.salir_button.pack(pady=10)
+
+        self.conexion = mysql.connector.connect(
+            host="127.0.0.1",  # Cambia esto a la dirección de tu servidor MySQL
+            user="root",
+            password="Emilio12345.",
+            database="neoguias"
+        )
+        self.cursor = self.conexion.cursor()
+
+        self.crear_tabla_si_no_existe()
+
+    def crear_tabla_si_no_existe(self):
+        self.cursor.execute("""
+            CREATE TABLE IF NOT EXISTS table_preguntas (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                pregunta TEXT,
+                respuesta_correcta TEXT,
+                opcion2 TEXT,
+                opcion3 TEXT,
+                opcion4 TEXT
+            )
+        """)
+        self.conexion.commit()
+
+    def agregar_pregunta(self):
+        pregunta = self.pregunta_entry.get()
+        respuesta_correcta = self.respuesta_correcta_entry.get()
+        opciones = [entry.get() for entry in self.opciones_entries if entry.get()]
+        
+        if pregunta and respuesta_correcta and opciones:
+            opciones.insert(0, respuesta_correcta)
+            self.cursor.execute("INSERT INTO table_preguntas (pregunta, respuesta_correcta, opcion2, opcion3, opcion4) VALUES (%s, %s, %s, %s, %s)",
+                                (pregunta, opciones[0], opciones[1], opciones[2], opciones[3]))
+            self.conexion.commit()
+            self.pregunta_entry.delete(0, tk.END)
+            self.respuesta_correcta_entry.delete(0, tk.END)
+            for entry in self.opciones_entries:
+                entry.delete(0, tk.END)
+            tk.messagebox.showinfo("Éxito", "Pregunta con opciones agregada correctamente.")
+        else:
+            tk.messagebox.showerror("Error", "Debes llenar todos los campos.")
+
+    def cerrar_app(self):
+        self.conexion.close()
+        self.root.destroy()
+    
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.geometry("400x400")
+    app = CrearPreguntaApp(root)
+    root.mainloop()
